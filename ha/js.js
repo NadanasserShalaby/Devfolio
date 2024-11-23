887597b9a1c0193958bd90ecc02562f84638a02d
NaDaNasser <shalabynadanasser@gmail.com>
2024-11-23 12:18:36 +0200

    Devfolio

---
+++ ha/js.js
@@ -0,0 +1,54 @@
+// function get_avr(x, y) {
+//     var x;
+//     var y;
+//     var avr = (x + y) / 2;
+//     console.log(avr);
+// }
+// get_avr(3, 3);
+
+
+// var user = {
+//     name: "Ola",
+//     age: 30,
+//     occupation: "SE",
+//     son: {
+//         name: "ahmed",
+//         son: {
+//             name: {
+//                 son: "ali"
+//             }
+//         }
+//     }
+// }
+// console.log(user.son.son.name.son);
+
+// var product = [
+//     {
+//         name: "oppo",
+//         "price": 1000,
+//     },
+//     {
+//         name: "iii",
+//         "price": 1000,
+//     },
+//     {
+//         name: "nada",
+//         "price": 1000,
+//     },
+//     {
+//         name: "iphone",
+//         "price": 1000,
+//     },
+//     {
+//         name: "nxm",
+//         "price": 1000,
+//     }
+// ]
+// console.log(product[0].name);
+// for (var i = 0; i < product.length; i++) {
+//     console.log(product[i].name);
+// }
+var na = ["nada", "nood", "mxc"];
+na.push("jxk")
+na.indexOf("mansn", 2)
+console.log(na);
